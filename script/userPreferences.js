96cf0f43ac31332a7604413e9186e1a0c1d67ee1
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-07-07 12:19:50 -0300

    User's Preferences Addition

---
+++ script/userPreferences.js
@@ -0,0 +1,17 @@
+function setGreeting(username) {
+    let currentHour = new Date().getHours();
+    if (currentHour < 12) {
+        return `Bom dia, ${username}!`;
+    } else if (currentHour < 18) {
+        return `Boa tarde, ${username}!`;
+    } else {
+        return `Boa noite, ${username}!`;
+    }
+}
+function renderGreeting(){
+    let greeting = setGreeting();
+    document.addEventListener('DOMContentLoaded', function(){
+        const greetingField = document.getElementById('user-greeting');
+        greetingField.innerHTML=`${greeting}`;
+    })
+}
